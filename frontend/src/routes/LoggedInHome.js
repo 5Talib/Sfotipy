54b4ae2dfb66a8a52b362c756d7998c8152405cd
Talib Khan <talibkhan@Talibs-MacBook-Air.local>
2024-08-29 15:47:25 +0530

    added loader

--- frontend/src/routes/LoggedInHome.js
+++ frontend/src/routes/LoggedInHome.js
@@ -5,7 +5,7 @@ import IconText from "../components/shared/IconText";
 import { Link, useNavigate } from "react-router-dom";
 import LoggedInContainer from "../containers/LoggedInContainer";
 import { makeAuthenicationGETRequest } from "../utils/serverHelpers";
-import { Rings } from "react-loader-spinner";
+import { Circles } from "react-loader-spinner";
 
 export default function LoggedInHome() {
   const [allArtists, setAllArtists] = useState([]);
@@ -46,7 +46,7 @@ export default function LoggedInHome() {
     <LoggedInContainer curActiveScreen={"home"}>
       {loading ? (
         <div className="flex justify-center items-center h-screen">
-          <Rings color="#00BFFF" height={100} width={100} />
+          <Circles color="#00BFFF" height={100} width={100} />
         </div>
       ) : (
         allArtists.map((artist) => {
